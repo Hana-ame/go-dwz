53fba78464b2aa1bede733d94b0386e827ad8d20
lumino <luminovoez@gmail.com>
2023-02-21 17:28:52 +0800

    #0.0.2 successfully add index

--- dwz/db.go
+++ dwz/db.go
@@ -38,7 +38,7 @@ func (o *Link) Read(db *gorm.DB, id string) (err error) {
 // to store the tag relationships.
 type Tag struct {
 	Tag string `gorm:"primaryKey;not null"`
-	Id  string `gorm:"primaryKey;not null"`
+	Id  string `gorm:"primaryKey;not null;index"`
 	Cnt int    `gorm:"not null;"` // times that the tag was tagged
 }
 
@@ -47,10 +47,15 @@ func (o *Tag) Create(db *gorm.DB) (err error) {
 	return tx.Error
 }
 
-func (o *Tag) Read(db *gorm.DB, id string) (err error) {
+func (o *Tag) Read(db *gorm.DB) (err error) {
 	tx := db.First(o)
 	return tx.Error
 }
+
+// gorm create index
+// https://gorm.io/docs/indexes.html
+// not null
+// https://stackoverflow.com/questions/43587610/preventing-null-or-empty-string-values-in-the-db
 
 // PRAGMA table_info('tags');
 // find that Tag is not primaryKey (SQLite)
--- dwz/db_test.go
+++ dwz/db_test.go
@@ -27,11 +27,19 @@ func TestTags(t *testing.T) {
 
 	db.Create(&Tag{"tag1-", "id1-", 0})
 	db.Create(&Tag{"tag1-", "id2-", 0})
+	db.Create(&Tag{"tag1-", "", 44})
 	db.Create(&Tag{"tag2-", "id1-", 0})
 	db.Create(&Tag{"tag2-", "id2-", 0})
 
-	tg := &Tag{Tag: "tag1-", Id: "id2-", Cnt: 5}
-	tg.Read(db, "")
+	// 可以使用多个键进行查找，但是查找到的东西是个谜。
+	// 可能是因为这里用的是复合主键的关系。总之试验了以下
+	// "", "", 44 可以找到。
+	// "", "id", xx 找到的是主键2的对应
+	// "tag", "id", xx 会汇报找得到找不到。
+	// tg := &Tag{Tag: "", Id: "id2-", Cnt: 44}
+	tg := &Tag{Tag: "tag3-", Id: "id3-", Cnt: 0}
+
+	fmt.Println(tg.Read(db))
 
 	fmt.Println(tg)
 
